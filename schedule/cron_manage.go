8a4b766fd47342ba36e5e94b4a0e60ae85eb8037
tomcat <cooge123@gmail.com>
2025-06-13 14:11:28 +0800

    Update cron_manage.go

--- schedule/cron_manage.go
+++ schedule/cron_manage.go
@@ -1,20 +1,27 @@
 package schedule
 
-import "github.com/robfig/cron/v3"
+import (
+	"github.com/robfig/cron/v3"
+	"sync"
+)
 
 type CronManage struct {
 	cronMap map[uint]cron.EntryID
 	cron    *cron.Cron
+	lock    *sync.Mutex
 }
 
 func NewCronManage() *CronManage {
 	return &CronManage{
 		cronMap: make(map[uint]cron.EntryID),
 		cron:    cron.New(cron.WithSeconds()),
+		lock:    new(sync.Mutex),
 	}
 }
 
 func (cronM *CronManage) Add(id uint, cron string, f func()) error {
+	cronM.lock.Lock()
+	defer cronM.lock.Unlock()
 	entryId, err := cronM.cron.AddFunc(cron, f)
 	if err != nil {
 		return err
