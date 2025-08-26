4fdc3c8c97e37759b92345bd2585926b57aa8edd
tomcat <cooge123@gmail.com>
2025-08-26 10:12:03 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,27 +6,15 @@ import (
 	"github.com/chuccp/smtp2http/core"
 	"github.com/chuccp/smtp2http/manage"
 	"github.com/chuccp/smtp2http/schedule"
-	"github.com/chuccp/smtp2http/unzip"
-	"log"
-	"strings"
 )
 
 func main() {
 
 	var webPort int
 	var apiPort int
-	var unzipFile string
 	flag.IntVar(&webPort, "web_port", 0, "web port")
 	flag.IntVar(&apiPort, "api_port", 0, "api port")
-	flag.StringVar(&unzipFile, "unzip", "", "unzip file dir")
 	flag.Parse()
-	if len(unzipFile) > 2 {
-		vs := strings.Split(unzipFile, " ")
-		err := unzip.Unzip(vs[0], vs[1])
-		if err != nil {
-			log.Panicln(err)
-		}
-	}
 	smtp2Http := core.Create()
 	smtp2Http.AddServer(manage.NewServer())
 	smtp2Http.AddServer(api.NewServer())
